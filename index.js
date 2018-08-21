22ed5f59f3601d7ffcc14e3625b7fb2fff5e8c7c
Ziqiang Li <ziqiang.lee@gmail.com>
2018-08-21 15:40:27 +0800

    option.include兼容字符串及其它兼容处理

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const compiler = require('vue-template-compiler');
 const transpile = require('vue-template-es2015-compiler');
 const loaderUtils = require('loader-utils');
 
-module.exports = function (content) {
+module.exports = function(content) {
     let options = loaderUtils.getOptions(this) || {};
+    let shouldCompile = false;
+    let include = options.include;
+    let resource = this.resource;
+    if (Array.isArray(include)) {
+        shouldCompile = include.some((v) => resource.startsWith(v));
+    } else if (typeof include === 'string') {
+        shouldCompile = resource.startsWith(include);
+    }
 
-    if (options.include.some((v) => this.resource.startsWith(v))) {
+    if (shouldCompile) {
         const compiled = compiler.compile(content);
 
         if (compiled.errors.length > 0) {
